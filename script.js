4ba1d38d87af58e9e5f261e68cb21bb913e14d43
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-02-09 20:46:28 +0100

    Add backdrop functionality to add task form
    The form and backdrop are toggled active/inactive together, improving the user interface and experience.

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ backBtn.addEventListener('click', toggleScreen);
 
 const addTaskBtn = document.querySelector('.add-task-btn');
 const addTaskForm = document.querySelector('.add-task');
+const blackBackdrop = document.querySelector('.black-backdrop');
 
 const toggleAddTaskForm = () => {
     addTaskForm.classList.toggle('active');
+    blackBackdrop.classList.toggle('active');
 };
+
+addTaskBtn.addEventListener('click', toggleAddTaskForm);
+blackBackdrop.addEventListener('click', toggleAddTaskForm);
